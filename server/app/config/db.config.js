b30b19e534707240a0360cf863c3985991327610
hinafi <quarkend@live.fr>
2021-05-31 08:25:42 +0200

    stash

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "password123",
+    PASSWORD: "password@22",
     DB: "db",
     dialect: "mysql",
-    // pool: {
-    //     max: 5,
-    //     min: 0,
-    //     acquire: 30000,
-    //     idle: 10000
-    // }
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 };
