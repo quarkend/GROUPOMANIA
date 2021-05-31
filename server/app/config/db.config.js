ff621e6bda431f6cad8830a0719ec416c1f9ee6e
hinafi <quarkend@live.fr>
2021-05-31 07:50:12 +0200

    react app

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,17 +1,13 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-<<<<<<< Updated upstream:server/config/db.config.js
     PASSWORD: "password123",
-=======
-    PASSWORD: "password@22",
->>>>>>> Stashed changes:server/app/config/db.config.js
     DB: "db",
     dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+    // pool: {
+    //     max: 5,
+    //     min: 0,
+    //     acquire: 30000,
+    //     idle: 10000
+    // }
 };
